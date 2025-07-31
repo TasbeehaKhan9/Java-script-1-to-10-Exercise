2d2589f60cb735d709f566b371cb32ec813d74b1
Tasbeeha Khan <tasbeehakhan05@gmail.com>
2025-08-01 02:21:16 +0500

    Initial commit for js excersise

---
+++ script.js
@@ -0,0 +1,60 @@
+// Chapter 1 alerts
+
+// question no 1 % 2
+
+alert("Error! plzz enter a valid password")
+console.log("Error! plzz enter a valid password");
+
+
+
+// question no 3
+
+alert("Welcome to JS land...\nHappy coding!")
+console.log("Welcome to JS land...<br>Happy coding!");
+
+
+// question no 4
+
+alert("Welcome to JS land...")
+console.log("Welcome to JS land...");
+
+alert("Happy coding!")
+console.log("Happy coding!");
+
+
+// question no 5
+
+alert("Hello I run JS through my web browser's console")
+console.log("Hello I run JS through my web browser's console");
+
+
+// chapter no 2 variable
+
+// question no 1
+
+var userName = ("jOHN DOE")
+console.log("jOHN DOE");
+
+// question no 2
+var name = ("Tasbeeha");
+console.log(name);
+
+// question no 3 ( a, b, c)
+
+var message; 
+message = "Hello World"
+alert("Hello World")
+console.log(message);
+
+// question no 4
+
+var studentName = ( "Tasbeeha");
+var studentAge = ("19");
+var studentCourse = ("Web & Application Development");
+
+alert(studentName);
+console.log ( "Tasbeeha")
+alert(studentAge);
+console.log ("19")
+alert(studentCourse);
+console.log ("Web & Application Development")
